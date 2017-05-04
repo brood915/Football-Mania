a13b94eaf5d56a528fbe6723ac381873b0b7dfa5
k <brood915@gmail.com>
2017-05-04 17:23:07 -0400

    added fetch + football-data.org API

--- src/containers/Leagues.js
+++ src/containers/Leagues.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Button } from 'react-bootstrap';
 
-const Leagues = () => (
-  <div className='mainContent'>
-    <h2>Leagues</h2>
-  </div>
-)
+class Leagues extends React.Component {
+
+	handleClick () {
+		fetch('http://api.football-data.org/v1/competitions', {
+  		headers: {
+    		'X-Auth-Token': '93ec85906d8a472894cad03fdadb19b9'
+  			}
+		})
+		.then((response) => response.json())
+		.then((json) => console.log(json[1].caption))
+		.catch((err) => console.log('Could not fetch the data!', err)
+  		)
+	}
+	
+	render () {
+		return (  
+		  <div className='mainContent'>
+		    <h2>Leagues</h2>
+		    <Button onClick = {this.handleClick.bind(this)}bsStyle='primary'>bring</Button>
+		  </div>
+	  );
+}
+}
+
 
 export default Leagues;
