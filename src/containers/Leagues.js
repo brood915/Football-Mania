1d3966fb5fed48d4084fca349125bdfc9f995a93
k <brood915@gmail.com>
2017-05-05 11:13:34 -0400

    added reusable getData function and passed to each route

--- src/containers/Leagues.js
+++ src/containers/Leagues.js
@@ -2,29 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
 
-class Leagues extends React.Component {
-
-	handleClick () {
-		fetch('http://api.football-data.org/v1/competitions', {
-  		headers: {
-    		'X-Auth-Token': '93ec85906d8a472894cad03fdadb19b9'
-  			}
-		})
-		.then((response) => response.json())
-		.then((json) => console.log(json[1].caption))
-		.catch((err) => console.log('Could not fetch the data!', err)
-  		)
-	}
-	
-	render () {
-		return (  
+const Leagues = (props) => (  
 		  <div className='mainContent'>
 		    <h2>Leagues</h2>
-		    <Button onClick = {this.handleClick.bind(this)}bsStyle='primary'>bring</Button>
+		    <Button onClick = {()=>props.getData('competitions')} bsStyle='primary'>bring</Button>
 		  </div>
 	  );
-}
-}
+
 
 
 export default Leagues;
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -7,19 +7,42 @@ import Leagues from './Leagues';
 import Tournaments from './Tournaments';
 import Players from './Players';
 import Saved from './Saved';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-
+import 'whatwg-fetch';
 
 class MainContainer extends React.Component {
+	constructor(props) {
+    super(props);
+    this.state = {
+    	
+    	}
+    this.getData = this.getData.bind(this);
+	}
+
+	getData (type) {
+		fetch('http://api.football-data.org/v1/' + type, {
+  		headers: {
+    		'X-Auth-Token': '93ec85906d8a472894cad03fdadb19b9'
+  			}
+		})
+		.then((response) => response.json())
+		.then((json) => console.log(json[1].caption))
+		.catch((err) => console.log('Could not fetch the data!', err)
+  		)
+	}
+
+	haha () {
+		console.log('haha');
+	}
 
 render() {
   return(<div id="main">
         <Header />
         <Route exact path="/" component={Home}/>
-        <Route path="/leagues" component={Leagues}/>
-        <Route path="/tournaments" component={Tournaments}/>
-        <Route path="/players" component={Players}/>
+        <Route path="/leagues" render={()=><Leagues getData = {this.getData}/>}/>
+        <Route path="/tournaments" render={()=><Tournaments getData = {this.getData}/>}/>
+        <Route path="/players" render={()=><Players getData = {this.getData}/>}/>
         <Route path="/saved" component={Saved}/>
         <Footer />
     </div>);
