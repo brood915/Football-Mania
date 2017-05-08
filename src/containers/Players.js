17adbd3cbc32f48d54b7c2b34ac8760b9f5a2f95
k <brood915@gmail.com>
2017-05-08 17:15:57 -0400

    added form. changed players container from stateless to stateful

--- src/containers/Players.js
+++ src/containers/Players.js
@@ -1,12 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Form from '../components/Form';
+import { Button, FormGroup, ControlLabel, FormControl, HelpBlock } from 'react-bootstrap';
 
 
-const Players = (props) => (
-  		  <div className='mainContent'>
-		    <h2>Players</h2>
-		  </div>
-)
+class Players extends React.Component {
+	constructor(props) {
+	    super(props);
+	    this.state = {
+	    	value: '',
+	    	validation: ''
+		}
+	}
 
+	handleValidation () {
+		const length = this.state.value.length;
+		if (length > 1 && length < 20) return 'success';
+		else if (length > 20) return 'warning';
+	    else if (0 < length && length < 2) return 'error';   
+	}
+
+	handleChange (e) {
+		this.setState({ value: e.target.value });
+	}
+
+	handleSubmit (e) {
+		e.preventDefault();
+		console.log('works')
+	}
+
+	render() { return (  
+	    	<div className='mainContent'>
+	      <h4>For now, only EPL players can be found.</h4>
+	      <form onSubmit = {this.handleSubmit.bind(this)}>
+	        <FormGroup 
+	        controlId="formBasicText"
+          	validationState={this.handleValidation()}>
+	          <ControlLabel>Find your favorite EPL players!</ControlLabel>
+	          <FormControl
+	            type="text"
+	            placeholder="Enter a player's first/last name"
+	            value={this.state.value}
+	            onChange={this.handleChange.bind(this)}
+	          />
+	          <FormControl.Feedback />
+	          <HelpBlock>Your input should be greater than 1 and less than 20.</HelpBlock>
+	        </FormGroup>
+	        <Button type='submit' onClick = {this.handleSubmit.bind(this)}>Get the player!</Button>
+	      </form>			   
+					
+			  </div>
+		  )
+		}
+}
 export default Players;
