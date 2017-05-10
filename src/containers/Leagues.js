08678b13c9def9ee7a215262910ac68454de9ffb
k <brood915@gmail.com>
2017-05-10 15:57:02 -0400

    added fixtures for leagues

--- src/containers/Leagues.js
+++ src/containers/Leagues.js
@@ -2,27 +2,52 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
 import Leagues_Table from '../components/Leagues_Table';
+import Leagues_Fixtures from '../components/Leagues_Fixtures';
 
-const Leagues = (props) => (  
-		  <div className='mainContent'>
-		    <form onChange = {props.getLeague}>
-				<FormGroup controlId="formControlsSelect">
-      				<ControlLabel>Select a league</ControlLabel>
-      				<FormControl componentClass="select" placeholder="select">
-      					<option defaultValue>Find your league!</option>
-				        <option value="426">English Premier League</option>
-				        <option value="430">Bundesliga</option>
-				        <option value="436">La Liga</option>
-				        <option value="438">Italian Serie A</option>
-				        <option value="434">France Ligue 1</option>
-				        <option value="433">Eredivisie</option>
-					</FormControl>
-				</FormGroup>
-      		</form>
-      		<Button onClick = {()=>props.getData('https://api.football-data.org/v1/competitions/', props.league + '/leagueTable')}>Get the table!</Button>
-      		<Leagues_Table data = {props.data} />
-		  </div>
-	  );
+class Leagues extends React.Component { 
+	constructor(props) {
+    super(props);
+    this.state = {
+ 		type: ''
+    	}
+	}	
+
+	handleClick (e) {
+		const type = e.target.getAttribute('data-type');
+		if (type === 'tables') {
+			this.props.getData('https://api.football-data.org/v1/competitions/', this.props.league + '/leagueTable');
+		}
+		else if (type === 'fixtures') {
+			this.props.getData('https://api.football-data.org/v1/competitions/', this.props.league + '/fixtures');
+		}
+		this.setState({type});
+	}
+
+    render() {
+  		return(<div className='mainContent'>
+		  		    <form onChange = {this.props.getLeague}>
+		  				<FormGroup controlId="formControlsSelect">
+		        				<ControlLabel>Select a league</ControlLabel>
+		        				<FormControl componentClass="select" placeholder="select">
+		        					<option defaultValue>Find your league!</option>
+		  				        <option value="426">English Premier League</option>
+		  				        <option value="430">Bundesliga</option>
+		  				        <option value="436">La Liga</option>
+		  				        <option value="438">Italian Serie A</option>
+		  				        <option value="434">France Ligue 1</option>
+		  				        <option value="433">Eredivisie</option>
+		  					</FormControl>
+		  				</FormGroup>
+		        		</form>
+		        		<div className='btnGroup'>
+		        		<Button data-type = 'tables' onClick = {this.handleClick.bind(this)}>Get the table!</Button>
+		        		<Button data-type = 'fixtures' onClick = {this.handleClick.bind(this)}>Upcoming Matches!</Button>
+		        		</div>
+		        		{this.state.type === 'tables' ? <Leagues_Table data = {this.props.data} /> : <Leagues_Fixtures data = {this.props.data} />}
+		        		
+		  		  </div>);
+}
+}
 
 
 
