d1405dd020229c3047287e14542e7510c9aa8882
k <brood915@gmail.com>
2017-05-15 15:51:15 -0400

    implemented a filter btn to get only upcoming fixtures

--- src/components/Teams_Fixtures.js
+++ src/components/Teams_Fixtures.js
@@ -3,74 +3,102 @@ import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
 
 
-const Teams_Fixtures = (props) => (
-	<div className='dataContainer'>
-	{props.data.fixtures && 
-		<div className = 'data'>
-		<h2>Fixtures</h2>
-		<div>
-			<span className = 'resultColor' style = {{'borderLeft': '10px solid blue'}}> = W</span>
-			<span className = 'resultColor' style = {{'borderLeft': '10px solid red'}}> = L</span>
-			<span className = 'resultColor' style = {{'borderLeft': '10px solid gray'}}> = D</span>
-		</div>
+class Teams_Fixtures extends React.Component {
+
+constructor(props) {
+    super(props);
+    this.state = {
+            upcoming: false
+      }
+}     
+
+
+filterFixtures () {
+      this.setState({upcoming:!this.state.upcoming});
+}
+
+
+render() {
+      return (
+      <div className='dataContainer'>
+      {this.props.data.fixtures && 
+            <div className = 'data'>
+            <h2>Fixtures</h2>
+            <div>
+                  <span className = 'resultColor' style = {{'borderLeft': '10px solid blue'}}> = W</span>
+                  <span className = 'resultColor' style = {{'borderLeft': '10px solid red'}}> = L</span>
+                  <span className = 'resultColor' style = {{'borderLeft': '10px solid gray'}}> = D</span>
+            </div>
             <div className='btnGroup infoBtn'>
                   <Button>Save</Button>
-                  <Button>Upcoming</Button>
+                  <Button onClick={this.filterFixtures.bind(this)}>
+                  {this.state.upcoming === false ? 'Upcoming' : 'Full'}
+                  </Button>
             </div>
-			<table>
-      			<tbody>
-	        		<tr>
-			          <th>Time</th>
-			          <th>Home</th>
-			          <th>Away</th>
-			          <th>Result</th>
-	          		</tr>
-      {props.data.fixtures.map((each,index) => { 
-			
-      		function getResult () {
-      			let color = '';
+                  <table>
+                        <tbody>
+                          <tr>
+                            <th>Time</th>
+                            <th>Home</th>
+                            <th>Away</th>
+                            <th>Result</th>
+                            </tr>
+      {this.props.data.fixtures
+            .filter(function (each,index) {
+                  if (this.state.upcoming === true) {
+                        return each.status !== 'FINISHED';
+                  }
+                  else {
+                        return each;
+                  }
+            }, this)
+            .map(function (each,index) { 
+                  
+                  const getResult = () => {
+                        let color = '';
 
-      	      	if (each.result.goalsHomeTeam !== each.result.goalsAwayTeam){
-      	      		let result = '';
+                        if (each.result.goalsHomeTeam !== each.result.goalsAwayTeam){
+                              let result = '';
 
-      	      		if (each.result.goalsHomeTeam > each.result.goalsAwayTeam){
-      	      		    result = each.homeTeamName;
-      	      		}
-      	      		else if (each.result.goalsHomeTeam < each.result.goalsAwayTeam){
-      	      			result = each.awayTeamName;
-      	      		}
+                              if (each.result.goalsHomeTeam > each.result.goalsAwayTeam){
+                                  result = each.homeTeamName;
+                              }
+                              else if (each.result.goalsHomeTeam < each.result.goalsAwayTeam){
+                                    result = each.awayTeamName;
+                              }
 
-      	      		color = result === props.name ? 'blue' : 'red';
-      	      	}
+                              color = result === this.props.name ? 'blue' : 'red';
+                        }
   
-      	      	else if (each.result.goalsHomeTeam !== null && each.result.goalsHomeTeam === each.result.goalsAwayTeam){
-      	      			color = 'gray';
-      	      	}
-      	      	
-      	      	else {
-      	      		color = 'black';
-      	      	}
+                        else if (each.result.goalsHomeTeam !== null && each.result.goalsHomeTeam === each.result.goalsAwayTeam){
+                                    color = 'gray';
+                        }
+                        
+                        else {
+                              color = 'black';
+                        }
 
-      	      	return color;			
-		}
+                        return color;                  
+            }
 
 
-      	return (
-            	<tr key = {index.toString()}>
-            		<td>{each.date}</td>
-            		<td>{each.homeTeamName}</td>
-            		<td>{each.awayTeamName}</td>
-            		<td style = {{'color': getResult()}}>{each.result.goalsHomeTeam !== null ? each.result.goalsHomeTeam + ':' + each.result.goalsAwayTeam : 'NA'}</td>
-            	</tr>
-      	      		); 
-      	      	}
-	     	 )
-	      }
-	            </tbody>
-	      	</table>
-		</div>
-	}
-	</div>
+            return (
+                  <tr key = {index.toString()}>
+                        <td>{each.date}</td>
+                        <td>{each.homeTeamName}</td>
+                        <td>{each.awayTeamName}</td>
+                        <td style = {{'color': getResult()}}>{each.result.goalsHomeTeam !== null ? each.result.goalsHomeTeam + ':' + each.result.goalsAwayTeam : 'NA'}</td>
+                  </tr>
+                              ); 
+                        },this)
+            }
+                  </tbody>
+                  </table>
+            </div>
+      }
+      </div>
 )
+}
+}
 
 export default Teams_Fixtures;
