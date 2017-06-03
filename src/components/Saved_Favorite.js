9544c6cf2ac7f3b3343c96e2184959160871af62
brood915 <brood915@gmail.com>
2017-06-03 00:32:34 -0400

    added logics to get diff data based on data-type attr

--- src/components/Saved_Favorite.js
+++ src/components/Saved_Favorite.js
@@ -6,7 +6,18 @@ import { Button, FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
 const Saved_Favorite = (props) => {
     const getLeague = (e) => {
         let league = e.target.value;
-        props.getLeague('https://api.football-data.org/v1/competitions/' + league + '/teams');
+        let dataType = e.target.getAttribute('data-type');
+
+        if (dataType === "leagues"){
+            console.log('leagues!')
+            props.getLeague('https://api.football-data.org/v1/competitions/' + league + '/teams');
+    
+        }
+
+        else {
+            console.log('teams!')
+        }
+        
     }
     
     return (
@@ -15,16 +26,16 @@ const Saved_Favorite = (props) => {
             <form onChange = {getLeague}>
                 <FormGroup controlId="formControlsSelect">
                     <ControlLabel>Select a league</ControlLabel>
-                        <Leagues_Form />
+                        <Leagues_Form dataType={'leagues'}/>
                     <ControlLabel>Select a team</ControlLabel>
-                    <FormControl componentClass="select" placeholder="select">
+                    <FormControl componentClass="select" placeholder="select" data-type='teams'>
                         <option defaultValue>Find your team!</option>
                         {props.teamNames.length > 0 && props.teamNames.map((each,index)=>(
 		        	<option key = {index.toString()} value = {each['_links'].self.href}>{each.name}</option>))}
                     </FormControl>
                 </FormGroup>
             </form>
-            <Button>Find</Button>
+            <Button>Add</Button>
         </div>
     );
 };
